d03305046c46e55ff513f3120ab37e8ef08765e3
Bastien <43323819+Batgame@users.noreply.github.com>
2025-08-06 19:49:06 +0200

    cleanup

--- src/hooks/useNFT.ts
+++ src/hooks/useNFT.ts
@@ -6,7 +6,7 @@ export function useNFT(id: string) {
     queryKey: ['nft', id],
     queryFn: () => api.getNFT(id),
     enabled: !!id,
-    placeholderData: keepPreviousData, // TanStack Query v5 - garde l'ancienne donnée
-    staleTime: 5 * 60 * 1000, // 5 minutes - évite les requêtes inutiles
+    placeholderData: keepPreviousData,
+    staleTime: 5 * 60 * 1000, // 5 minutes
   });
 }
